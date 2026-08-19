25a09370861b7c3e46b8594e4a6b9f72dbad8f97
LeonardoDJ <serlepe147leo@gmail.com>
2026-08-19 13:57:44 -0300

    configura trusted proxies para Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->preventRequestForgery(except: [
-        'contatos',
-    ]);
+            'contatos',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
